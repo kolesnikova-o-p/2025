cf88b80f2dd58ea272993a4fc49c53e3b2c29193
Olga <Kolesnikova.OP@yandex.ru>
2025-10-17 01:41:26 +0300

    17.10.2025

---
+++ SEPMP/math_.py
@@ -0,0 +1,22 @@
+class SimpleMath:
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @staticmethod
+    def subtract(a, b):
+        return a - b
+
+    @staticmethod
+    def multiply(a, b):
+        return a * b
+
+    @staticmethod
+    def divide(a, b):
+        if b == 0:
+            raise ValueError("Деление на ноль невозможно")
+        return a / b
+
+    @staticmethod
+    def power(a, b):
+        return a ** b
