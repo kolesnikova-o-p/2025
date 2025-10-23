c68faf0257bd8372c726631daa6bac709d28103b
Olga <Kolesnikova.OP@yandex.ru>
2025-10-24 02:49:44 +0300

    24.10.2025

---
+++ SEPMP/without_class.py
@@ -0,0 +1,27 @@
+class Pet:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+
+    def __str__(self):
+        return f"{self.name} ({self.species})"
+
+
+class Owner:
+    def __init__(self, name):
+        self.name = name
+        self.pets = []
+
+    def add_pet(self, pet):
+        self.pets.append(pet)
+
+    def __str__(self):
+        pets_list = ", ".join(str(pet) for pet in self.pets)
+        return f"{self.name} питомцы: {pets_list}"
+
+owner = Owner("Наташа")
+cat = Pet("Мурка", "Кошка")
+dog = Pet("Бобик", "Собака")
+owner.add_pet(cat)
+owner.add_pet(dog)
+print(owner)
